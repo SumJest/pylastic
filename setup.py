e4aa05da50e5cb45553600646c615a8b864a36e7
SumJest <summedjesters@gmail.com>
2023-11-27 16:34:10 +0500

    add requirements

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ import pylastic
 VERSION = pylastic.__version__
 DESCRIPTION = 'Tensor Analysis Library'
 LONG_DESCRIPTION = 'Library for analyzing the crystal stiffness tensor'
-with open('requirements.txt', 'r') as fio:
-    REQUIREMENTS = fio.read().split('\n')
+
 # Setting up
 setup(
     # the name must match the folder name 'verysimplemodule'
@@ -16,6 +15,6 @@ setup(
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     packages=find_packages(),
-    install_requires=REQUIREMENTS,
-
+    install_requires=['aiohttp~=3.8.5', 'pydantic~=2.4.2', 'Mako~=1.2.4', 'numpy~=1.26.0', 'scipy~=1.11.2',
+                    'requests~=2.31.0', 'plotly~=5.18.0', 'setuptools'],
 )

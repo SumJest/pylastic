7730a06b89ac4c4b61e5fa8ba82f0a4038f85ca3
SumJest <summedjesters@gmail.com>
2023-11-28 20:51:56 +0500

    add .html files by setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = 'Library for analyzing the crystal stiffness tensor'
 setup(
     # the name must match the folder name 'verysimplemodule'
     name="pylastic",
-    version='0.1.2',
+    version='0.1.3',
     author="Roman Fakhrutdinov",
     author_email="<summedjesters@gmail.com>",
     description=DESCRIPTION,
@@ -16,4 +16,5 @@ setup(
     install_requires=['aiohttp~=3.8.5', 'pydantic~=2.4.2', 'Mako~=1.2.4', 'numpy~=1.26.0', 'scipy~=1.11.2',
                     'requests~=2.31.0', 'plotly~=5.18.0', 'setuptools'],
     include_package_data=True,
+    package_data={'templates': ['pylastic/elater/viewer/templates/*.html', 'pylastic/elater/renderer/templates/*.html']}
 )

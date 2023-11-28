25cba6dbe136b1fffbaf119e38fbe888b1d29686
SumJest <summedjesters@gmail.com>
2023-11-28 21:15:25 +0500

    return Manifest.in

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = 'Library for analyzing the crystal stiffness tensor'
 setup(
     # the name must match the folder name 'verysimplemodule'
     name="pylastic",
-    version='0.1.4',
+    version='0.1.5',
     author="Roman Fakhrutdinov",
     author_email="<summedjesters@gmail.com>",
     description=DESCRIPTION,
@@ -15,6 +15,5 @@ setup(
     packages=find_packages(),
     install_requires=['aiohttp~=3.8.5', 'pydantic~=2.4.2', 'Mako~=1.2.4', 'numpy~=1.26.0', 'scipy~=1.11.2',
                     'requests~=2.31.0', 'plotly~=5.18.0', 'setuptools'],
-    include_package_data=True,
-    package_data={'templates': ['pylastic/elater/viewer/templates/*.html', 'pylastic/elater/renderer/templates/*.html']}
+    include_package_data=True
 )

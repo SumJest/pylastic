d1acb20f2071ad3a502c2eaef9c98010e5386a94
SumJest <summedjesters@gmail.com>
2023-11-28 21:07:25 +0500

    update 0.1.4

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ LONG_DESCRIPTION = 'Library for analyzing the crystal stiffness tensor'
 setup(
     # the name must match the folder name 'verysimplemodule'
     name="pylastic",
-    version='0.1.3',
+    version='0.1.4',
     author="Roman Fakhrutdinov",
     author_email="<summedjesters@gmail.com>",
     description=DESCRIPTION,

0652deaac0ac62d33b8f245b004c391d6d34172b
SumJest <summedjesters@gmail.com>
2023-12-25 14:46:38 +0500

    fix ElasticFunction

--- pylastic/elater/renderer/functions.py
+++ pylastic/elater/renderer/functions.py
@@ -10,3 +10,14 @@ class ElasticFunction(enum.Enum):
     def __init__(self, num, title):
         self.num = num
         self.title = title
+
+    @staticmethod
+    def by_num(num: int):
+        for member in ElasticFunction:
+            if member.num == num:
+                return member
+        raise ValueError(f"{num} is not a valid ElasticFunction")
+
+    @staticmethod
+    def by_name(name: str):
+        return getattr(ElasticFunction, name)
